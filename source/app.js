9972f8b5f6599344c7b9e7872a1b4cc9b0664108
Amit Ashutosh <amitashutosh@hotmail.com>
2015-10-06 14:21:51 -0700

    Added examples to app.js file

--- source/app.js
+++ source/app.js
@@ -40,6 +40,10 @@ ReactDOM.render(listOfItems, document.getElementById('react-application'));
 
 */
 
+/*
+
+//Example 3
+
 var listItemElement1 = React.DOM.li({ className: 'item-1', key: 'item-1' }, 'Item1');
 var listItemElement2 = React.DOM.li({ className: 'item-2', key: 'item-2' }, 'Item2');
 var listItemElement3 = React.DOM.li({ className: 'item-3', key: 'item-3' }, 'Item3');
@@ -48,5 +52,37 @@ var reactFragment = [ listItemElement1, listItemElement2, listItemElement3 ];
 var listOfItems = React.DOM.ul({ className: 'list-of-items' }, reactFragment);
 
 ReactDOM.render(listOfItems, document.getElementById('react-application'));
+
+*/
+
+/*
+
+// Example 4 - JSX example
+
+
+var listOfItems = <ul className="list-of-items">
+                    <li className="item-1">Item 1</li>
+                    <li className="item-2">Item 2</li>
+                    <li className="item-3">Item 3</li>
+                   </ul>;
+
+ReactDOM.render(listOfItems, document.getElementById('react-application'));
+
+*/
+
+
+
+
+// Example 4 - Creating Reach component
+
+ var ReactClass = React.createClass({
+ render: function () {
+ return React.createElement('h1', { className: 'header' }, 'React Component');
+ }
+ });
+
+ var reactComponentElement = React.createElement(ReactClass);
+ var reactComponent = ReactDOM.render(reactComponentElement, document.getElementById('react-application'));
+
 
 
